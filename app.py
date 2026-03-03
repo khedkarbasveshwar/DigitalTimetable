a1cf7169caa55634598f125863aa8aa5ad99388a
basveshwar <2022bcs106@sggs.ac.in>
2026-03-03 15:17:57 +0530

    Initial commit of Digital Timetable project

--- app.py
+++ app.py
@@ -160,5 +160,8 @@ def generate_timetable():
                            timetable=timetable_data)
 
 # ---------------- Main ----------------
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
